8b483ec3d0e8b5bfdba529bd6e1b3897d3171c45
Lucas Farias <lucasfarias14@outlook.com>
2025-02-06 13:45:13 -0300

    Teste 1 concluido

---
+++ Teste1/Model/ContaBancaria.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Teste1.Model
+{
+    public class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string? NomeTitular { get; set; }
+        public decimal DepositoInicial { get; private set; }
+        public decimal SaldoConta { get; private set; }
+
+        private const decimal Taxa = 3.50m;
+
+
+        public ContaBancaria(int numeroConta, string nomeTitular, decimal depositoInicial)
+        {
+            this.NumeroConta = numeroConta;
+            this.NomeTitular = nomeTitular;
+            this.DepositoInicial = depositoInicial;
+            this.SaldoConta = this.DepositoInicial;
+        }
+
+        public ContaBancaria(int numeroConta, string? nomeTitular)
+        {
+            this.NumeroConta = numeroConta;
+            this.NomeTitular = nomeTitular;
+            this.SaldoConta = 0.0m;
+            this.DepositoInicial = this.SaldoConta;
+        }
+
+
+        public void Depositar(decimal valor)
+        {
+            this.SaldoConta += valor;
+        }
+
+        public string Sacar(decimal valor)
+        {
+            if(valor <= this.SaldoConta)
+            {
+                this.SaldoConta -= valor;
+                this.SaldoConta -= Taxa;
+                return $"Foi realizado o saque no valor de {valor} da conta do titular {this.NomeTitular}";
+            }
+
+            return "Saldo insuficiente para saque";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Dados da conta atualizado:");
+            sb.Append($"Conta {this.NumeroConta}, Titular: {this.NomeTitular}, Saldo: $ {this.SaldoConta.ToString(CultureInfo.InvariantCulture)}");
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Teste1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using Teste1.Model;
+
+ContaBancaria conta;
+
+Console.Write("Entre o número da conta: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.Write("Entre o titular da conta: ");
+string? nomeTitular = Console.ReadLine();
+Console.Write("Haverá o deposito inicial (s/n)? ");
+char resp = Convert.ToChar(Console.ReadLine());
+
+if (resp == 's' || resp == 'S')
+{
+    Console.Write("Entre o valor de depósito inicial: ");
+    decimal depositoInicial = Convert.ToDecimal(Console.ReadLine(), CultureInfo.InvariantCulture);
+    conta = new ContaBancaria(numero, nomeTitular, depositoInicial);
+}
+else
+    conta = new ContaBancaria(numero, nomeTitular);
+
+Console.WriteLine();
+Console.WriteLine(conta);
+
+Console.WriteLine();
+Console.Write("Entre um valor para depósito: ");
+decimal valor =  Convert.ToDecimal(Console.ReadLine(), CultureInfo.InvariantCulture);
+conta.Depositar(valor);
+Console.WriteLine(conta);
+
+Console.WriteLine();
+Console.Write("Entre um valor para saque: ");
+valor = Convert.ToDecimal(Console.ReadLine(),CultureInfo.InvariantCulture);
+string saldoAtualizadoPosSaque = conta.Sacar(valor);
+Console.WriteLine($"\n{saldoAtualizadoPosSaque}\n");
+Console.WriteLine(conta);
